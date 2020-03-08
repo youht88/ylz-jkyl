1a58202fbb71941cfe6606faa494d605c1e7a30f
youht88 <youht88@ylzinfo.com>
2020-03-09 01:14:03 +0800

    fix bug ,there app.js shuould not be async function

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const SMcrypto = require("/utils/smcrypto.js").SMcrypto
 const crypto = new SMcrypto()
 const util = require("/utils/util.js").util
 App({
-  onLaunch: async function() {
+  onLaunch: function() {
     // 本地密钥体系
     console.log("keys:",crypto)
     // 登录
@@ -51,8 +51,9 @@ App({
     //this.globalData.baseURL="http://web1.imac1.youht.cc:8084"
     //this.globalData.baseURL = "http://192.168.31.119:6001"
     //this.globalData.baseURL = "http://10.10.0.199:5000"
-    let temp = await util.getStorage("baseURL")
-    this.globalData.baseURL = temp
+    util.getStorage("baseURL").then(x=>{
+      this.globalData.baseURL = x
+    })
     this.globalData.IconAnimal ="https://dss1.bdstatic.com/6OF1bjeh1BF3odCf/it/u=4064283291,520199009&fm=74&app=80&f=JPEG&size=f121,90?sec=1880279984&t=b24f80de85ff357047609f47d65dc3b3"
     this.globalData.IconGirl ="http://pics.sc.chinaz.com/Files/pic/icons128/rw_7/Vivian%20Chow.png"
     this.globalData.IconSelf = ""
